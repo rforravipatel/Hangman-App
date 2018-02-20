b39a0161286eaba02f91ef84242a074852156d18
rforravipatel <rforravipatel@gmail.com>
2018-02-20 00:42:30 -0500

    selection of random word done

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 var app = angular.module("HangmanApp",[]);
 app.controller("GameController",['$scope',function($scope){
-	$scope.test = "Helllo World";
-	console.log($scope.test);
+	
+	var randomWords = ["jigar","jeel","ruchi","ravi","joy","fenil"];
+	$scope.incorrectWords = [];
+	$scope.correctWords = [];
+	$scope.guesses = 6;
+	$scope.displayWord = '';
+	$scope.input = {
+		letter : ''
+	}
+
+	var randomWord = function(){
+		var index = Math.round(Math.random()*randomWords.length);
+		return randomWords[index];
+	}
+
+	var gamestart = function(){
+		
+		$scope.incorrectWords = [];
+		$scope.correctWords = [];
+		$scope.guesses = 6;
+		$scope.displayWord = '';
+
+		selectedWord = randomWord();
+		console.log(selectedWord);
+	}
+
+	gamestart();
+
 }]);
